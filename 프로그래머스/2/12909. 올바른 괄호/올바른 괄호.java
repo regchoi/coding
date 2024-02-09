db2fd0d59f9b374d92ca5577b68b6212bff9daa4
regchoi <103176657+regchoi@users.noreply.github.com>
2024-02-10 02:27:56 +0900

    [level 2] Title: 올바른 괄호, Time: 20.09 ms, Memory: 52.6 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,28 +1,27 @@
 import java.util.*;
 
 class Solution {
-    boolean solution(String s) {
+     boolean solution(String s) {
         boolean answer = true;
         Stack<Character> stack = new Stack<>();
-        
-        for(int i = 0; i < s.length(); i++){
+
+        for (int i = 0; i < s.length(); i++) {
             char a = s.charAt(i);
-            
-            if(a == '('){
+
+            if (a == '(') {
                 stack.push(a);
-            }
-            
-            if(a == ')'){
-                if(stack.isEmpty()){
+            } else if (a == ')') {
+                if (stack.isEmpty()) {
                     stack.push(a);
                 } else {
-                  stack.pop();  
-                } 
+                    stack.pop();
+                }
             }
         }
         if (!stack.isEmpty()) {
             answer = false;
         }
+
         return answer;
     }
 }
